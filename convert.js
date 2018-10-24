5300d46ad4fabc632fc8b8f36a878444db2d8fee
Kathrin Reissner <kathrin.reissner11@gmail.com>
2018-10-24 11:48:16 +0200

    Finished

--- convert.js
+++ convert.js
@@ -1,1 +1,9 @@
-console.log('Not implemented yet!');
+const convert = require('convert-units');
+console.log('Units to Convert\n' + convert().possibilities() + '\n');
+if(isNaN(process.argv[2]) || process.argv[4] != "to"){
+    console.log('Invalid');
+}
+else{
+    console.log('Convert:\n' + process.argv[2] + ' ' + process.argv[3] + ' ' + process.argv[4] + ' ' + process.argv[5]);
+    console.log('New Value: ' + convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]));
+}
